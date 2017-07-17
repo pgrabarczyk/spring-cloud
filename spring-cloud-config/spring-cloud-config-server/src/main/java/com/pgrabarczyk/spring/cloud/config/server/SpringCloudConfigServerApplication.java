22defb5dc2578b12e15f39e87aeb87082449cadd
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2017-07-17 11:27:20 +0200

    spring-cloud-config-server init

---
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/pgrabarczyk/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.pgrabarczyk.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
